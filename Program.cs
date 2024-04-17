fcd15d6e526a6c3b8eebb11f6ef39168c946cee7
S. David Kyle <sdavidkyle@gmail.com>
2024-04-16 19:03:12 -0700

    Add Select extension method to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿IEnumerable<int> source = new int[] { 1, 2, 3, 4, 5 };
+foreach (int i in Select(source, x => x * 2))
+{
+    Console.WriteLine(i);
+}
+
+
+static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+{
+    foreach (TSource item in source)
+    {
+        yield return selector(item);
+    }
+}
+
+
