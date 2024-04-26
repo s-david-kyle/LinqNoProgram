b281ce195c926e2b19f50e60775cca52fc2c88d0
S. David Kyle <sdavidkyle@gmail.com>
2024-04-25 20:25:47 -0700

    Implimenting IEnumerable

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿using System.Security.Cryptography;
+﻿using System.Collections;
 
-Console.WriteLine(0);
-IEnumerable<int> e = Select<int, int>(null, x => x * 2);
-Console.WriteLine(1);
-IEnumerator<int> enumerator = e.GetEnumerator();
-Console.WriteLine(2);
-_ = enumerator.MoveNext();
+IEnumerable<int> source = Enumerable.Range(1, 1000).ToArray();
+Console.WriteLine(Enumerable.Select(source, x => x * 2).Sum());
+Console.WriteLine(Select(source, x => x * 2).Sum());
 
-static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+
+
+
+static IEnumerable<TResult> SelectCompiler<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
 
     ArgumentNullException.ThrowIfNull(source);
@@ -25,4 +25,83 @@ static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source
 
 }
 
+static IEnumerable<TResult> SelectManual<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+{
+
+    ArgumentNullException.ThrowIfNull(source);
+    ArgumentNullException.ThrowIfNull(selector);
+
+    return new SelectManualEnumerable<TSource, TResult>(source, selector);
+
+    // static IEnumerable<TResult> Impl(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    // {
+    //     foreach (TSource item in source)
+    //     {
+    //         yield return selector(item);
+    //     }
+    // }
+
+}
+
+sealed class SelectManualEnumerable<TSource, TResult> : IEnumerable<TResult>
+{
+    private readonly IEnumerable<TSource> _source;
+    private readonly Func<TSource, TResult> _selector;
+
+    public SelectManualEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    {
+        _source = source;
+        _selector = selector;
+    }
+
+    public IEnumerator<TResult> GetEnumerator()
+    {
+        return new Enumerator(_source, _selector);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private sealed class Enumerator : IEnumerator<TResult>
+    {
+        private readonly IEnumerable<TSource> _source;
+        private readonly Func<TSource, TResult> _selector;
+        private TResult _current = default!;
+
+        public Enumerator(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            _source = source;
+            _selector = selector;
+        }
+
+        public TResult Current => _current;
+
+        object? IEnumerator.Current => Current;
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            IEnumerator<TSource> enumerator = _source.GetEnumerator();
+            try
+            {
+                if (enumerator.MoveNext())
+                {
+                    _current = _selector(enumerator.Current);
+                    \
+                }
+            }
+            finally
+            {
+                enumerator?.Dispose();
+            }
+
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
 
